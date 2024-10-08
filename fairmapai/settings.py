9a2647b5358c7bff06e2cbc15f0545bf989303ea
wcia013twe <wcia013twe@example.com>
2024-10-08 01:34:18 -0400

    Renamed project and updated database settings

--- fairmapai/settings.py
+++ fairmapai/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        default='postgresql://postgres:postgres@localhost:5432/censusdb',
         conn_max_age=600
     )
 }
